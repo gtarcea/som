4ec8a9f3ab33357d4daebb9f60dbf30a6aa0a32f
gtarcea <glenn.tarcea@gmail.com>
2019-07-08 21:54:21 -0400

    Start fleshing out the parser

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,34 @@
 package parser
+
+import (
+	"github.com/gtarcea/som/internal/lexer"
+	"github.com/gtarcea/som/internal/token"
+	"github.com/hashicorp/go-multierror"
+)
+
+type Parser struct {
+	l            *lexer.Lexer
+	errors       multierror.Error
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l: l,
+	}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) Parse() {
+	return
+}
